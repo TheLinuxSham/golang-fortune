ab37cdc6b4672d743c88b2ec3adf76aecd753fee
Jan Schneider <j.schn@tutanota.com>
2024-10-10 09:23:40 +0200

    update refactor code

--- internal/fortune/fortune.go
+++ internal/fortune/fortune.go
@@ -3,34 +3,39 @@ package fortune
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 )
+
+type Fortunes struct {
+	Fortunes []Fortune `json:"fortunes"`
+}
 
 type Fortune struct {
 	Animal string   `json:"animal"`
 	Quotes []string `json:"quotes"`
 }
 
-type Content struct {
-	Content []Fortune `json:"content"`
+func openJson() *Fortunes {
+	file, err := os.Open("fortunes.json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var fortunes Fortunes
+	err = json.NewDecoder(file).Decode(&fortunes)
+	if err != nil {
+		panic(err)
+	}
+
+	return &fortunes
 }
 
 func FortuneTeller() {
-	file, err := os.Open("fortunes.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	quotes := openJson()
 
-	var quotes Content
-	err = json.NewDecoder(file).Decode(&quotes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Quotes:")
-	for _, quote := range quotes.Content {
+	for _, quote := range quotes.Fortunes {
 		fmt.Printf("\nQuotes from %s:\n", quote.Animal)
 		for _, q := range quote.Quotes {
 			fmt.Printf("\t%s\n", q)
