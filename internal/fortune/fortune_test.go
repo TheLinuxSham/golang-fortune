a47eb02557b64a96a07a3d510fb932ef2812ca69
Jan Schneider <j.schn@tutanota.com>
2024-10-10 10:52:39 +0200

    update new test

--- internal/fortune/fortune_test.go
+++ internal/fortune/fortune_test.go
@@ -3,25 +3,51 @@ package fortune
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"testing"
 )
 
 func mockingJson() string {
 	jsonData := `{
-			"turtle": [
-				"There is strength in taking it slow... and I’m really strong!",
-				"Slow and steady gets you there – with way less stress.",
-				"Haste makes waste, but really... let waste win.",
-			],
-			"wolf": [
-				"A lone wolf still howls at the moon, even when no one is listening.",
-				"Strength doesn’t come from numbers; it comes from the fight within.",
-				"The pack protects, but the lone wolf thrives in silence.",
-				]
-			}`
+    "fortunes": [
+        {
+            "animal": "turtle",
+            "quotes": [
+                "There is strength in taking it slow... and I’m really strong!",
+                "Slow and steady gets you there – with way less stress.",
+                "Haste makes waste, but really... let waste win."
+            ]
+        },
+        {
+            "animal": "wolf",
+            "quotes": [
+                "A lone wolf still howls at the moon, even when no one is listening.",
+                "Strength doesn’t come from numbers; it comes from the fight within.",
+                "The pack protects, but the lone wolf thrives in silence."
+            ]
+        }
+    ]
+}`
 
 	return jsonData
+}
+
+func mockingOpenJson() *Fortunes {
+	file, err := os.Open("../../fortunes.json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var fortunes Fortunes
+	err = json.NewDecoder(file).Decode(&fortunes)
+	if err != nil {
+		panic(err)
+	}
+
+	return &fortunes
 }
 
 func TestOpenJson(t *testing.T) {
@@ -39,8 +65,16 @@ func TestOpenJson(t *testing.T) {
 	}
 }
 
-func TestFortuneTeller(t *testing.T) {
-	quotes := openJson()
+func TestJsonFormat(t *testing.T) {
+	var fortunes Fortunes
+	err := json.Unmarshal([]byte(mockingJson()), &fortunes)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestFJsonFile(t *testing.T) {
+	quotes := mockingOpenJson()
 
 	for _, quote := range quotes.Fortunes {
 		fmt.Printf("\nQuotes from %s:\n", quote.Animal)
