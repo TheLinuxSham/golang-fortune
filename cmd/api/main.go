545bfc54f58b26b6e7d8fdf9fd52d61e82786f32
Jan Schneider <j.schn@tutanota.com>
2024-10-12 22:58:36 +0200

    update implement server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/TheLinuxSham/golang-fortune/internal/fortune"
+import (
+	"log"
+	"net/http"
+
+	"github.com/TheLinuxSham/golang-fortune/internal/server"
+)
 
 func main() {
-	fortune.FortuneTeller()
+	server := server.NewServer()
+
+	log.Println("Starting server on :3030")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Could not listen on :3030: %v\n", err)
+	}
 }
